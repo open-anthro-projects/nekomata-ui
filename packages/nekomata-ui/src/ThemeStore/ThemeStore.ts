10c4db44818d6635f1225adf592f40b99f6ab6b2
Tom Konink <konink1992@gmail.com>
2020-10-25 17:27:13 +0100

    ThemeStore added exports need fixing

--- packages/nekomata-ui/src/ThemeStore/ThemeStore.ts
+++ packages/nekomata-ui/src/ThemeStore/ThemeStore.ts
@@ -1,6 +1,6 @@
 import { writable } from 'svelte/store';
 
-export let themeKey = "nekomata-ui-theme"
+let themeKey = "nekomata-ui-theme"
 
 export function changeThemeKey(key:string){
     themeKey = key;
