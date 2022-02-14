12de57235dc2430f16195bbd69c57c386123cff6
Sanctuscoma <konink1992@gmail.com>
2022-02-14 23:16:32 +0100

    website update

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,6 +7,7 @@
 <style>
     :global(body) {
         margin: 0px;
+        font-family: Roboto, sans-serif;
     }
 
     .nekomata-ui{
@@ -29,6 +30,24 @@
         --nm-ui-overlay-display: block;
         --nm-ui-flex-box-column-align-items: stretch;
     }
+
+    a{
+        -webkit-font-smoothing: antialiased;
+        font-family: Roboto, sans-serif;
+        font-size: 1.25rem;
+        line-height: 2rem;
+        font-weight: 500;
+        font-weight: 500;
+        letter-spacing: 0.0125em;
+        text-decoration: inherit;
+        text-transform: inherit;
+        padding-left: 20px;
+        padding-right: 0;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        overflow: hidden;
+        z-index: 1;
+    }
 </style>
 
 <div class:drawer={drawerVisible === true} class="nekomata-ui">
@@ -37,6 +56,7 @@
             <Button on:click={() => drawerVisible = !drawerVisible}>
                 <SvgIcon d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z"></SvgIcon>
             </Button>
+            <a href="/">Nekomata-ui</a>
         </FlexBox>
     </AppBar>
     <Overlay on:click={() => drawerVisible = !drawerVisible}/>
@@ -47,6 +67,7 @@
                     <Button on:click={() => drawerVisible = !drawerVisible}>
                         <SvgIcon d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z"></SvgIcon>
                     </Button>
+                    <a href="/">Nekomata-ui</a>
                 </FlexBox>
             </AppBar>
             <ul>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,77 +1,35 @@
 <script lang="ts">
-    import { GridContainer, AccordionPanel} from 'nekomata-ui'
-
-    let panelList = new Array();
-
-    let panel: AccordionPanel;
-
-    let test = false;
-
-    let list = [{
-		id: 0,
-        style: "--max-height: 100%"
-	}
-    ,{
-		id: 1,
-        style: null
-	}, {
-		id: 2,
-        style: null
-	}, {
-		id: 3,
-        style: null
-	}, {
-		id: 4,
-        style: null
-	}, {
-		id: 5,
-        style: null
-	}, {
-		id: 6,
-        style: "--max-height: 100%"
-	}];
-
-
-    function maxheight(panel: HTMLElement) {
-        if (panel.style.getPropertyValue("--nm-ui-accordion-panel-max-height")) {
-            panel.style.setProperty("--nm-ui-accordion-panel-max-height" , null);
-        } else {
-        panel.style.setProperty("--nm-ui-accordion-panel-max-height" , 100 + "%");
-        } 
-    }
-
+    import { GridContainer} from 'nekomata-ui';
 </script>
 
 <style>
     .grid {
 		padding: 64px 0px 0px 0px;
         --nm-ui-grid-container-grid-template-areas:
-		'. . w w w w w w w w . .';
+		'. . w w w w w w w w . .'
+        '. . b b b b b b b b . .';
 	}
+
+    h1{
+        font-size:3.75em;
+        margin: 60px 0px 0px 0px;
+    }
 
     .item{
         grid-area: w;
-        --nm-ui-accordion-panel-active-max-height: 100%;
-    }
-
-    h1, p {
-        text-align:center;
-    }
-
-    .panel {
-        padding: 0 18px;
-        background-color: white;
-        max-height: var(--max-height, 0);
-        overflow: hidden;
-        transition: max-height 0.5s ease-out;
     }
 
     @media only screen and (max-width: 600px){
         .grid {
             padding: 56px 0px 0px 0px;
             --nm-ui-grid-container-grid-template-areas:
-		'w w w w w w w w w w w w';
+		'w w w w w w w w w w w w'
+        'b b b b b b b b b b b b';
         }
+    }
+
+    .box{
+        grid-area: b;
     }
 </style>
 
@@ -80,53 +38,10 @@
 <div class="grid">
     <GridContainer>
         <div class="item">
-            <h1>Welcome to Nekomata-ui</h1>
-            <p>This website is under construction!</p>
-
-        
-<!-- 
-        <button on:click={() => maxheight(panel.exportHTMLElement())}>click me</button> -->
-
-        <button on:click={() => test = !test}>click me</button>
-
-        <AccordionPanel active={test}>
-            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-
-            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
- 
-            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
- 
-            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
- 
-            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
- 
-            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
- 
-            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
- 
-            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        </AccordionPanel>
-
-        
-
-        <!-- <div class="panel" bind:this={panelList[item.id]} style={item.style}>
-           <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-
-           <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-
-           <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-
-           <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-
-           <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-
-           <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-
-           <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-
-           <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-        </div> -->
-
-    </div>
+            <h1>Nekomata-ui</h1>
+            <p>Svelte UI component library that focuses on delivering components and features that can be adaptable to your application needs.</p>
+        </div>
+        <div class="box">
+        </div>
     </GridContainer>
 </div>
